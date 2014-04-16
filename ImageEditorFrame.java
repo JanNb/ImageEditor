13b7d1a163e291e38835e225fbf8bbf2410b2e62
JanNb <jan.niebuhr@haw-hamburg.de>
2014-04-16 14:32:04 +0200

    Open-Menu

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -1,4 +1,5 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.event.*;
 
 
 public class ImageEditorFrame extends JFrame {
@@ -7,7 +8,25 @@ public class ImageEditorFrame extends JFrame {
 		setSize(400, 300);
 		setVisible(true);
 	}
-
+	
+	private void createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		JMenuItem menuItemOpen = new JMenuItem("Open");
+		menuFile.add(menuItemOpen);
+		menuItemOpen.addActionListener(
+			new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					onOpen();
+				}
+			}
+		);
+	}
+	private void onOpen() {
+		JOptionPane.showMessageDialog(this, "Open Selected");
+	}
 	
 	public static void main(String[] args) {
 		ImageEditorFrame editorFrame = new ImageEditorFrame();
